52833ee894c52fb047dc5c2603798d22af642e00
jtaylor515 <jeffrey.taylor@emory.edu>
2023-11-02 16:18:53 -0400

    Automation Completion

--- updateScript.py
+++ updateScript.py
@@ -12,16 +12,13 @@ input_nb_files = [
 ]
 
 # Output directory for Python script files
-output_directory = "python_scripts"
+output_directory = os.getcwd()  # Use the current working directory
 
 def extract_code_cells(input_file, output_directory):
-    # Create the output directory if it doesn't exist
-    os.makedirs(output_directory, exist_ok=True)
-
     with open(input_file, "r") as notebook_file:
         notebook = nbformat.read(notebook_file, as_version=4)
 
-    output_file = os.path.join(output_directory, os.path.splitext(os.path.basename(input_file))[0] + ".py")
+    output_file = os.path.splitext(os.path.basename(input_file))[0] + ".py"
 
     with open(output_file, "w") as python_file:
         for cell in notebook.cells:
@@ -34,5 +31,9 @@ def extract_code_cells(input_file, output_directory):
 if __name__ == "__main__":
     for input_nb_file in input_nb_files:
         extract_code_cells(input_nb_file, output_directory)
+
+    # Remove the "python_scripts" directory if it exists
+    if os.path.exists("python_scripts"):
+        shutil.rmtree("python_scripts")
 
     print("Files are updated. RUNME.py can continue running")
